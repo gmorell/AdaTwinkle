4cb47e458815546caedfb9d28887a1cb5010d014
gmorell <gmp@h4xmb.org>
2014-11-16 14:36:12 -0500

    more work on HSV state storage

--- led_states.py
+++ led_states.py
@@ -1,50 +1,21 @@
 from helpers import HSVHelper
 
+
 class BaseLEDState(object):
     def __init__(self, r=0, g=0, b=0, step_size=3, id=0):
         self.r = r
         self.g = g
         self.b = b
         self.sz = step_size
-        
+
         self.r_t = 0
         self.g_t = 0
         self.b_t = 0
         self.id = id
 
-    def at_zeroes(self): # for fading up or down
-        return self.r == self.g == self.b == 0        
-
-    def read(self):
-        return [self.r, self.g, self.b]
-    
-    def read_t(self):
-        return [self.r_t, self.g_t, self.b_t]
-        
-class DumbRGBLEDState(BaseLEDState):
-    """
-    Steps around with no regard to maintaining consistent hues or anything else really
-    """
-    
-    def at_target(self): # to write the next values
-        if self.r == self.r_t and self.g == self.g_t and self.b == self.b_t:
-            return True
-        else:
-            return False
-
-    
-    def set_step_target(self,r,g,b):
-        self.r_t = r
-        self.g_t = g
-        self.b_t = b
-        
-    def set_step_target_to_zeroes(self):
-        self.r_t = 0
-        self.g_t = 0
-        self.b_t = 0
-        
-    def _step(self,current,dest):
-        if abs(current-dest) > self.sz:
+    # generic step function
+    def _step(self, current, dest):
+        if abs(current - dest) > self.sz:
             if current > dest:
                 current -= self.sz
             elif current < dest:
@@ -54,13 +25,98 @@ class DumbRGBLEDState(BaseLEDState):
         else:
             current = dest
         return current
-        
+
+    def at_target(self):
+        raise NotImplementedError("Need to implement a stop function")
+
+    def at_zeroes(self):  # for fading up or down
+        raise NotImplementedError("Need to implement a reverse function")
+
+    def read(self):
+        raise NotImplementedError("Need to implement a read for internal state")
+
+    def read_rgb(self):
+        raise NotImplementedError("Need to implement a read for internal state (converted to RGB)")
+
+    def read_t(self):
+        raise NotImplementedError("Need to implement a read for internal target state")
+
+    def read_t_rgb(self):
+        raise NotImplementedError("Need to implement a read for internal target state (converted to RGB)")
+
+
+class DumbRGBLEDStepState(BaseLEDState):
+    """
+    Steps around with no regard to maintaining consistent hues or anything else really
+    """
+
+    def at_target(self):  # to write the next values
+        if self.r == self.r_t and self.g == self.g_t and self.b == self.b_t:
+            return True
+        else:
+            return False
+
+    def at_zeroes(self):  # for fading up or down
+        return self.r == self.g == self.b == 0
+
+    def set_step_target(self, r, g, b):
+        self.r_t = r
+        self.g_t = g
+        self.b_t = b
+
+    def set_step_target_to_zeroes(self):
+        self.r_t = 0
+        self.g_t = 0
+        self.b_t = 0
+
     def do_step(self):
         self.r = self._step(self.r, self.r_t)
         self.g = self._step(self.g, self.g_t)
         self.b = self._step(self.b, self.b_t)
 
-            
+    def read(self):
+        return [self.r, self.g, self.b]
+
+    def read_rgb(self):
+        return [self.r, self.g, self.b]
+
+    def read_t(self):
+        return [self.r_t, self.g_t, self.b_t]
+
+    def read_t_rgb(self):
+        return [self.r_t, self.g_t, self.b_t]
 
 
-class HSVAwareLEDState(BaseLEDState, HSVHelper):
+class HSVAwareLEDStepState(BaseLEDState, HSVHelper):
+    def __init__(self, h, s, v, step_size=3, id=0):
+        self.h = h
+        self.s = s
+        self.v = v
+        self.sz = step_size
+        self.id = id
+
+        self.h_t = 0
+        self.s_t = 0
+        self.v_t = 0
+
+    def at_zeroes(self):
+        return self.s == self.v == 0
+
+    def do_step(self):
+        self.h = self._step(self.h, self.h_t)
+        self.s = self._step(self.s, self.s_t)
+        self.v = self._step(self.v, self.v_t)
+
+    def read(self):
+        return [self.h, self.s, self.v]
+
+    def read_rgb(self):
+        r, g, b = self._hsv_to_rgb(self.h, self.s, self.v)
+        return [r, g, b]
+
+    def read_t(self):
+        return [self.h_t, self.s_t, self.v_t]
+
+    def read_t_rgb(self):
+        r, g, b = self._hsv_to_rgb(self.h_t, self.s_t, self.v_t)
+        return [r, g, b]
--- twinkle.py
+++ twinkle.py
@@ -4,70 +4,18 @@ import serial
 import time
 
 # vars
+from led_states import DumbRGBLEDStepState,HSVAwareLEDStepState
+
 LED_COUNT = 240
 LED_PORT = "/dev/ttyACM0"
 LED_DURATION = 600
 LED_FADE_TIME = 0.05
 LED_FADE_STEPS = 30
-        
-class LEDState(object):
-    def __init__(self, r=0, g=0, b=0, step_size=3, id=0):
-        self.r = r
-        self.g = g
-        self.b = b
-        self.sz = step_size
-        
-        self.r_t = 0
-        self.g_t = 0
-        self.b_t = 0
-        self.id = id
-    
-    def at_target(self): # to write the next values
-        if self.r == self.r_t and self.g == self.g_t and self.b == self.b_t:
-            return True
-        else:
-            return False
-        
-    def at_zeroes(self): # for fading up or down
-        return self.r == self.g == self.b == 0
-    
-    def set_step_target(self,r,g,b):
-        self.r_t = r
-        self.g_t = g
-        self.b_t = b
-        
-    def set_step_target_to_zeroes(self):
-        self.r_t = 0
-        self.g_t = 0
-        self.b_t = 0
-        
-    def _step(self,current,dest):
-        if abs(current-dest) > self.sz:
-            if current > dest:
-                current -= self.sz
-            elif current < dest:
-                current += self.sz
-            else:
-                pass
-        else:
-            current = dest
-        return current
-        
-    def do_step(self):
-        # simplest step algo, will add an HSV mode later.
-        self.r = self._step(self.r, self.r_t)
-        self.g = self._step(self.g, self.g_t)
-        self.b = self._step(self.b, self.b_t)
 
-            
-    def read(self):
-        return [self.r, self.g, self.b]
-    
-    def read_t(self):
-        return [self.r_t, self.g_t, self.b_t]
 
 class LEDStringTwinkle(object):
-    def __init__(self, led_count, run_duration, fade_time, fade_steps, device, twinkle_hues = [], fadeout=True, debug=False):
+    def __init__(self, led_count, run_duration, fade_time, fade_steps, device, twinkle_hues=[], fadeout=True,
+                 debug=False, state=DumbRGBLEDStepState):
         self.led_count = led_count
         self.run_duration = run_duration
         self.fade_time = fade_time
@@ -76,56 +24,56 @@ class LEDStringTwinkle(object):
         self.device = device
         self.fadeout = fadeout
         self.debug = debug
-        
+
         # figure out when to stop
         self.t_start = time.time()
         self.t_end = self.t_start + self.run_duration
-        
+
         # create objects for tracking LED states
-        self.leds = [LEDState(id=i) for i in range(self.led_count)]
-        
+        self.leds = [state(id=i) for i in range(self.led_count)]
+
         # create the send buffer
         buffer_start = ['\x41', '\x64', '\x61']
-        buffer_3 = (self.led_count -1) >> 8
-        buffer_4 = (self.led_count -1) & 0xff
+        buffer_3 = (self.led_count - 1) >> 8
+        buffer_4 = (self.led_count - 1) & 0xff
         buffer_5 = buffer_3 ^ buffer_4 ^ 0x55
         self.buffer_complete = buffer_start + [buffer_3, buffer_4, buffer_5]
-    
+
     def dprint(self, string):
         if self.debug:
             print(string)
+
     def run(self):
         while time.time() < self.t_end:
             new_buffer = deepcopy(self.buffer_complete)
             for led in self.leds:
-                if led.at_target(): # we set values here
+                if led.at_target():  # we set values here
                     self.dprint("@target")
-                    if led.at_zeroes(): # we input new colors here
+                    if led.at_zeroes():  # we input new colors here
                         self.dprint("@zeroes")
                         # for now just twinkle to some random orange, we'll do hue juggling next
                         led.set_step_target(
-                            random.randint(192,255),
-                            random.randint(0,64),
-                            0, # random.randint(0,128),
+                            random.randint(192, 255),
+                            random.randint(0, 64),
+                            0,  # random.randint(0,128),
                         )
-               
+
                     else:
                         led.set_step_target(
-                            0,0,0
+                            0, 0, 0
                         )
-                
-                
+
                 led.do_step()
-                new_buffer.extend(led.read())
-                
+                new_buffer.extend(led.read_rgb())
+
             self.device.write(new_buffer)
             time.sleep(self.fade_time)
-        
+
         # fadeout
         if self.fadeout:
             for l in self.leds:
-                l.set_step_target(0,0,0)
- 
+                l.set_step_target(0, 0, 0)
+
             while not all([l.at_zeroes() for l in self.leds]):
                 new_buffer = deepcopy(self.buffer_complete)
                 for led in self.leds:
@@ -133,22 +81,20 @@ class LEDStringTwinkle(object):
                         pass
                     else:
                         led.do_step()
-                    new_buffer.extend(led.read())
-                        
+                    new_buffer.extend(led.read_rgb())
+
                 self.device.write(new_buffer)
                 time.sleep(self.fade_time)
-            
 
 
-s = serial.Serial(LED_PORT,115200)
+s = serial.Serial(LED_PORT, 115200)
 
-
-t = LEDStringTwinkle(LED_COUNT, LED_DURATION, LED_FADE_TIME, LED_FADE_STEPS, s, [0])
+t = LEDStringTwinkle(LED_COUNT, LED_DURATION, LED_FADE_TIME, LED_FADE_STEPS, s, [0], state=HSVAwareLEDStepState)
 t.run()
 
 s.close()
 
-###
+# ##
 
 ## TODO
 # LED_HOLD : int: hold a value at its peak for x cycles. (remember to reset the hold after its done)
